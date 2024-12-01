a510cae50bbe093ed678f5212636dbb55c18485c
Manicka Raj <96897008+RbY-Rj@users.noreply.github.com>
2024-12-01 14:39:27 +0530

    Update password.py

--- plugins/Extra/password.py
+++ plugins/Extra/password.py
@@ -19,5 +19,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('VJ Bots', url='https://t.me/vj_bots')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('RbyLinkzz ⚡', url='https://t.me/addlist/su-tJBv_e9VlN2Nl')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
