102b18c965348bc5079ea9bead92727ade70a98b
Manicka Raj <96897008+RbY-Rj@users.noreply.github.com>
2025-05-25 12:12:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/459b05de9d75612fe00aa.jpg')).split()
 
 
 # Admins & Users
